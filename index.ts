d77ee38d7d1f98e281390a1e6d49d4d0b3578f39
Ryusei Yamaguchi <mandel59@gmail.com>
2025-04-13 20:42:10 +0900

    Implement SIGINT handling

--- index.ts
+++ index.ts
@@ -1,62 +1,10 @@
 #!/usr/bin/env bun
-import Bun from "bun";
-import { name, version } from "./package.json";
-import app, { host, port, imagesDir, development } from "./backend/app.ts";
-import finder from "./frontend/finder/index.html";
-import { resolve } from "node:path";
-import os from "node:os";
-import { isIP } from "node:net";
+import serve from "./server.ts";
 
-function serverHostname() {
-  let h = host;
-  let m;
-  if ((m = host.match(/^\[(.*)\]$/)) && isIP(m[1]!) === 6) {
-    h = m[1]!;
-  }
-  switch (isIP(h)) {
-    case 6:
-      if (/^[0:]*$/.test(h)) {
-        return os.hostname();
-      }
-      return `[${h}]`
-    case 4:
-      if (/^[0\.]*$/.test(h)) {
-        return os.hostname();
-      }
-      return h;
-    default:
-      return h;
-  }
-}
+const server = serve();
 
-function serverUrl() {
-  const u = new URL("http://invalid/");
-  const p = parseInt(port);
-  if (!(0 < p && p <= 0xffff)) {
-    throw new Error("Invalid port");
-  }
-  u.hostname = serverHostname();
-  u.port = p.toString();
-  if (u.hostname === "invalid") {
-    throw new Error("Invalid host");
-  }
-  return u;
-}
-
-console.log(`${name} ${version}`);
-console.log(`Powered by Bun ${Bun.version_with_sha}`);
-console.log(`Serving images from ${resolve(imagesDir)}`);
-
-const u = serverUrl();
-
-const server = Bun.serve({
-  hostname: u.hostname,
-  port: port,
-  routes: {
-    "/.be/*": app.fetch,
-    "/*": finder,
-  },
-  development,
+process.once("SIGINT", async () => {
+  console.log("Received SIGINT");
+  await server.stop();
+  console.log("Server stopped");
 });
-
-console.log(`Server is running at ${u.href}`);
