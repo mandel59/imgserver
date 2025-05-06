f26dc813792d08efc7ff377a376d4065bb70b667
Ryusei Yamaguchi <mandel59@gmail.com>
2025-05-06 22:20:11 +0900

    Implement reloading button

--- frontend/finder/Controls.tsx
+++ frontend/finder/Controls.tsx
@@ -1,11 +1,23 @@
-import { useAtom } from "jotai";
-import { sortOptionAtom } from "./states.ts";
+import { useAtom, useAtomValue } from "jotai";
+import { currentFileItemsQueryAtom, sortOptionAtom } from "./states.ts";
 import type { SortOption } from "@/common/types.ts";
+import { FaRedo } from "react-icons/fa";
 
 export default function Controls() {
   const [sortOption, setSortOption] = useAtom(sortOptionAtom);
+  const { refetch: refetchCurrentFileItems, isFetching } = useAtomValue(
+    currentFileItemsQueryAtom
+  );
   return (
     <div id="controls">
+      <button 
+        onClick={() => refetchCurrentFileItems()}
+        disabled={isFetching}
+        className="refresh-button"
+        aria-label={isFetching ? "読み込み中" : "ファイル一覧を更新"}
+      >
+        <FaRedo className={isFetching ? "spin" : ""} />
+      </button>
       <select
         id="sort-option"
         value={sortOption}
